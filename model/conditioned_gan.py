263510deb576a4eb6d4add9321323ea2c4ff157c
malcolmyanguci <gufengy@uci.edu>
2021-02-08 01:38:33 -0800

    add Transformer and Play function

--- model/conditioned_gan.py
+++ model/conditioned_gan.py
@@ -11,10 +11,14 @@ import pdb
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 import math
 
-
-def get_model(NB_GENRES = 9,len_input= 100,len_seq= 32,nb_notes=9,sequence_model ="Transformer"):
-    return (generator(NB_GENRES,len_input,len_seq,nb_notes,sequence_model),
-            discriminator(NB_GENRES,len_input,len_seq,nb_notes,sequence_model))
+def get_model(NB_GENRES = 9,
+                noise_len= 100,
+                len_seq= 32,
+                nb_notes=9,
+                kernal ="LSTM"
+                ):
+    return (discriminator(NB_GENRES,len_seq,nb_notes,kernal),
+            generator(NB_GENRES,noise_len,len_seq,nb_notes,kernal))
 
 def get_loss(cuda = True):
     print(cuda)
@@ -50,14 +54,6 @@ class conditioned_gan_loss(torch.nn.Module):
         return loss
 
 
-
-def generate_square_subsequent_mask(sz):
-    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-    return mask
-
-
-# Temporarily leave PositionalEncoding module here. Will be moved somewhere else.
 class PositionalEncoding(nn.Module):
     r"""Inject some information about the relative or absolute position of the tokens
         in the sequence. The positional encodings have the same dimension as
@@ -82,6 +78,7 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
@@ -97,212 +94,165 @@ class PositionalEncoding(nn.Module):
         Examples:
             >>> output = pos_encoder(x)
         """
-
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
 
+class TransformerModel(nn.Module):
+
+    def __init__(self, input_channels = 128, nhead = 4, nhid = 2048, nlayers = 3, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        self.model_type = 'Transformer'
+
+        self.pos_encoder = PositionalEncoding(input_channels, dropout)
+        encoder_layers = TransformerEncoderLayer(input_channels, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.input_channels = input_channels
+
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        #sz should be length of sequence
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        # self.decoder.bias.data.zero_()
+        # self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask = None):
+        #src should be [sequence length, batch size, embed dim]
+        src = src * math.sqrt(self.input_channels)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        # output = self.decoder(output)
+        return output
+
 class discriminator(nn.Module):
-    def __init__(self,NB_GENRES = 9,len_input= 100,len_seq= 32,nb_notes=9,sequence_model ="Transformer"):
+    def __init__(self,
+                NB_GENRES = 9,
+                len_seq= 32,
+                nb_notes=9,
+                kernal = 'LSTM'):
         super(discriminator, self).__init__()
         self.NB_GENRES = NB_GENRES
-        self.len_input = len_input
         self.len_seq = len_seq
         self.nb_notes = nb_notes
-        self.sequence_model = sequence_model
+        self.kernal = kernal
 
+        self.embedding_block = nn.Sequential(
+                                        torch.nn.Embedding(NB_GENRES,np.prod((len_seq, nb_notes))))
 
-        self.embedding1 = torch.nn.Embedding(NB_GENRES,np.prod((len_seq, nb_notes)))
+        if(kernal == "Transformer"):
+            self.kernal_block = TransformerModel(input_channels = 18,nhead = 6,nlayers = 2,dropout=0.3)
+            self.kernalout = torch.nn.Linear(18, 128)
+        elif(kernal == "LSTM"):
+            self.kernal_block = torch.nn.LSTM(18,hidden_size = 64,num_layers = 2, batch_first = False,bidirectional = True)
+
+        self.MLP_block = nn.Sequential(
+                                    torch.nn.Dropout(0.3),
+                                    torch.nn.Linear(128, 512),
+                                    torch.nn.LeakyReLU(0.01),
+                                    torch.nn.Dropout(0.3),
+                                    torch.nn.Linear(512, 256),
+                                    torch.nn.LeakyReLU(0.01),
+                                    torch.nn.Linear(256, 1))
         
-
-        #LSTM block
-        #=========================================
-        if(self.sequence_model  =="LSTM"):
-
-            self.bidirectional1 = torch.nn.LSTM(18,hidden_size = 64,batch_first = True,bidirectional = True)
-            self.bidirectional2 = torch.nn.LSTM(128,hidden_size = 64,batch_first = True,bidirectional = True)
-            
-            self.drop1 = torch.nn.Dropout(0.3)
-            self.l1 = torch.nn.Linear(128, 512)
-            self.LeakyReLU1 = torch.nn.LeakyReLU(0.01)
-            
-            self.drop2 = torch.nn.Dropout(0.3)
-            self.l2 = torch.nn.Linear(512, 256)
-            self.LeakyReLU2 = torch.nn.LeakyReLU(0.01)
-            self.l3 = torch.nn.Linear(256, 1)
-        #=========================================
-
-        
-        
-        # #transfermer block
-        #=========================================
-        if(self.sequence_model  =="Transformer"):
-            self.src_mask = None
-            self.pos_encoder = PositionalEncoding(18, dropout = 0.1)
-            self.encoder_layers = TransformerEncoderLayer(18, nhead = 2 , dim_feedforward = 2048, dropout = 0.1)
-            self.transformer_encoder = TransformerEncoder(self.encoder_layers, num_layers=2)
-            self.decoder = torch.nn.Linear(18, 1)
-            self.decoder2 = torch.nn.Linear(32, 1)
-        #=========================================
-
         self.sg = torch.nn.Sigmoid()
 
 
-    def forward(self, x,label,has_mask = True):
+    def forward(self, x,label):
 
         B = x.shape[0]
-        label = self.embedding1(label)
-        label = label.view(-1,self.len_seq, self.nb_notes)
-        x = torch.cat([x,label],axis = 2)
+        label = self.embedding_block(label)#(B,1)->(B,1,288)
+        label = label.squeeze(1) #(B,1,288)->(B,288)
+        label = label.view(B,self.len_seq, self.nb_notes)#(B,288)->(B,32,9)
+        x = torch.cat([x,label],axis = 2)#(B,32,9),(B,32,9)->(B,32,18)
 
-
-
-
-        #LSTM block
-        # #=========================================
-        if(self.sequence_model  =="LSTM"):
-            x,_= self.bidirectional1(x)
-            x,_ = self.bidirectional2(x)
-            x = x[:,-1,:].view(B,-1)
-            x = self.drop1(x)
-            x = self.l1(x)
-            x = self.LeakyReLU1(x)
-
-            x = self.drop2(x)
-            x = self.l2(x)
-            x = self.LeakyReLU2(x)
-            x = self.l3(x)
+        #Kernal block
+        #=========================================
+        x = x.permute(1,0,2)#(B,32,18)->(32,B,18)
+        
+        if(self.kernal == "Transformer"):
+            x = self.kernal_block(x)
+            x = self.kernalout(x)
+        elif(self.kernal == "LSTM"):
+            x,_ = self.kernal_block(x)
+        
+        x = x.permute(1,0,2)#(32,B,128)->(B,32,128)
+        x = x[:,-1,:].view(B,-1)#Take the last output: (B,32,128)->(B,1,128)->(B,128)
         #=========================================
 
-
-                
-        # #transfermer block
-        #=========================================
-        if(self.sequence_model  =="Transformer"):
-            if has_mask:
-                device = x.device
-                if self.src_mask is None or self.src_mask.size(0) != len(x):
-                    mask = generate_square_subsequent_mask(len(x)).to(device)
-                    self.src_mask = mask
-            else:
-                self.src_mask = None
-
-
-            x = self.pos_encoder(x)
-            x = self.transformer_encoder(x, self.src_mask)
-            x = self.decoder(x)
-            x = self.decoder2(x.view(B,-1))
-        #=========================================
-
-
-        x = self.sg(x) 
+        x = self.MLP_block(x)#(B,128) -> (B,1)
+        x = self.sg(x)
         return x
 
 
 class generator(nn.Module):
-    def __init__(self,NB_GENRES = 9,len_input= 100,len_seq= 32,nb_notes=9,sequence_model ="Transformer"):
+    def __init__(self,NB_GENRES = 9,
+                 noise_len= 100,
+                 len_seq= 32,
+                 nb_notes=9,
+                 kernal = 'LSTM'):
+        
         super(generator, self).__init__()
         self.NB_GENRES = NB_GENRES
-        self.len_input = len_input
+        self.noise_len = noise_len
         self.len_seq = len_seq
         self.nb_notes = nb_notes
-        self.sequence_model = sequence_model
+        self.kernal = kernal
 
+        self.embedding_block = nn.Sequential(
+                                            torch.nn.Embedding(NB_GENRES,noise_len))
 
-        self.embedding1 = torch.nn.Embedding(NB_GENRES,len_input)        
-        self.l1 = torch.nn.Linear(len_input, 512)
-        self.LeakyReLU1 = torch.nn.LeakyReLU(0.2)
-        self.bn1 = torch.nn.BatchNorm1d(512, momentum=0.9)
-
-        self.l2 = torch.nn.Linear(512, 1024)
-        self.LeakyReLU2 = torch.nn.LeakyReLU(0.2)
-        self.bn2 = torch.nn.BatchNorm1d(1024, momentum=0.9)
+        self.MLP_block = nn.Sequential(        
+                                    torch.nn.Linear(noise_len, 512),
+                                    torch.nn.LeakyReLU(0.2),
+                                    torch.nn.BatchNorm1d(512, momentum=0.9),
+                                    torch.nn.Linear(512, 1024),
+                                    torch.nn.LeakyReLU(0.2),
+                                    torch.nn.BatchNorm1d(1024, momentum=0.9)
+                                    )
 
         self.drop1 = torch.nn.Dropout(0.3)
 
-
-
-        #LSTM block
-        # #=========================================
-        if(self.sequence_model  =="LSTM"):
-
-            self.LSTM1 = torch.nn.LSTM(32,hidden_size = 128,batch_first = True,bidirectional = False)
-            self.LSTM2 = torch.nn.LSTM(128,hidden_size = 128,batch_first = True,bidirectional = False)
-            self.LSTM3 = torch.nn.LSTM(128,hidden_size = 9,batch_first = True,bidirectional = False)
-        
-        # #=========================================
-
-        
-        
-        # #transfermer block
-        # #=========================================
-        if(self.sequence_model  =="Transformer"):
-
-            self.src_mask = None
-            self.pos_encoder = PositionalEncoding(32, dropout = 0.1)
-            self.encoder_layers = TransformerEncoderLayer(32, nhead = 2 , dim_feedforward = 2048, dropout = 0.1)
-            self.transformer_encoder = TransformerEncoder(self.encoder_layers, num_layers=3)
-            self.decoder = torch.nn.Linear(32, 9)
-        
-        # #=========================================
-
+        if(kernal == "Transformer"):
+            self.kernal_block = TransformerModel(input_channels = 32,nhead = 8,nlayers = 3,dropout=0.3)
+            self.kernalout = torch.nn.Linear(32, nb_notes)
+        elif(kernal == "LSTM"):
+            self.kernal_block = torch.nn.LSTM(32,hidden_size = 128,num_layers = 3,batch_first = False,bidirectional = False)
+            self.kernalout = torch.nn.Linear(128, nb_notes)
 
         self.sg = torch.nn.Sigmoid() 
 
 
-    def forward(self, x,label,has_mask=True):
+    def forward(self, x,label):
 
         B = x.shape[0]
-        label = self.embedding1(label)
+        label = self.embedding_block(label)#(B,1)->(B,1,100)
+        label = label.squeeze(1)#(B,1,100)->(B,100)
+        x = torch.mul(x,label)#(B,100),(B,100)->(B,100)
 
-        label = label.view(B,self.len_input)
-
-        x = torch.mul(x,label)
-
-        x = self.l1(x) 
-        x = self.LeakyReLU1(x) 
-        x = self.bn1(x) 
-
-        x =self.l2(x)  
-        x =self.LeakyReLU2(x) 
-        x =self.bn2(x)  
-        x = x.view(B,32,32)
+        x = self.MLP_block(x)#(B,100)->(B,1024)
+        x = x.view(B,32,32)#(B,1024) ->(B,32,32)
         x = self.drop1(x) 
 
+        #Kernal block
+        #=========================================
+        x = x.permute(1,0,2)#(B,32,32)->(32,B,32)
 
 
-        #LSTM block
-        # #=========================================
-        if(self.sequence_model =="LSTM"):
-            x,_ =self.LSTM1(x)  
-            x,_ =self.LSTM2(x)  
-            x,_ =self.LSTM3(x) 
-        # #=========================================
+        if(self.kernal == "Transformer"):
+            x = self.kernal_block(x)
+            x = self.kernalout(x)
+        elif(self.kernal == "LSTM"):
+            x,_ = self.kernal_block(x)
+            x = self.kernalout(x)
 
-
-
-
-        # #transfermer block
-        # #=========================================
-        if(self.sequence_model  =="Transformer"):
-            if has_mask:
-                device = x.device
-                if self.src_mask is None or self.src_mask.size(0) != len(x):
-                    mask = generate_square_subsequent_mask(len(x)).to(device)
-                    self.src_mask = mask
-            else:
-                self.src_mask = None
-            x = self.pos_encoder(x)
-            x = self.transformer_encoder(x, self.src_mask)
-            x = self.decoder(x)
-        # #=========================================
-
-
-
-
+        x = x.permute(1,0,2)#(32,B,128)->(B,32,128)
+        #=========================================
 
         x = self.sg(x) 
-
-
-
         return x
